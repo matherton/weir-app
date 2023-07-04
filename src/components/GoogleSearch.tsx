cc7c0791bcc3bfd479031fae3a9c87b28afed362
Mark Atherton <mark@insightinternet.co.uk>
2023-07-04 13:36:43 +0100

    initial vite/styled-components setup and got a dummy GoogleSearch component to render in App

---
+++ src/components/GoogleSearch.tsx
@@ -0,0 +1,10 @@
+/**
+ * Renders a Google search component.
+ *
+ * @param props - The props required for the GoogleSearch component.
+ * @returns The rendered GoogleSearch component.
+ */
+export default function GoogleSearch() {
+  // Render the GoogleSearch component
+  return <div>googleSearch</div>;
+}
